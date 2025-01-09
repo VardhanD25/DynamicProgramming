e3589ce9d3e8b9b7a28e2ca585f68516aef61b2a
Vardhan Dongre <vardhandongre67@gmail.com>
2025-01-09 19:15:33 +0530

    More problems on subsequences

--- Leetcode/115_Distinct_Subsequences.py
+++ Leetcode/115_Distinct_Subsequences.py
@@ -14,4 +14,19 @@ class Solution(object):
                 else:
                     dp[i][j]=dp[i-1][j]
         return dp[n][m]
+    
+#1D Space optimised
+
+class Solution(object):
+    def numDistinct(self, str1, str2):
+        n=len(str1)
+        m=len(str2)
+        prev=[0]*(m+1)
+
+        prev[0]=1
+        for i in range(1,n+1):
+            for j in range(m,0,-1):
+                if str1[i-1]==str2[j-1]:
+                    prev[j]=prev[j-1]+prev[j]
+        return prev[m]
         
